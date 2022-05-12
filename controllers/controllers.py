6171667fa9c1942251f5b9be6f3d97db136c0f4d
crccode <54750603+crccode@users.noreply.github.com>
2022-05-12 17:13:22 -0400

    j1

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-import json
-from datetime import datetime, date
+
 from odoo import http
 from odoo.http import request
 
@@ -9,9 +8,15 @@ _logger = logging.getLogger(__name__)
 
 class OdooContr4ollers(http.Controller):
 	# La ruta ni la funcion se puede repetir dentro de esta clase podemos tener muchas pero solo un "get_products"
-	# public Para que podamos acceder sin registrarno
+	# type = 'json' TANTO LA CONXION COMO LA DEVOLUCION NOS DEVOLVERA JSON
+	# auth = 'public' SI NO LE PONES PUBLIC TENDRAS QUE PONER USER, CONTRASEÑA
+	# sudo() SOLO LA LINEA DE CODIGO TENDRA ACCESO DE SUPER USUARIO
 	@http.route(['/get_products'], type='json', auth='public',  website=True)
 	def get_products(self, **post):
+		name = post.get('name', False)
+		name1 = post.get('name1', False)
+
+		print(name+ '---------'+ name1)
 		print('python')
 		products = http.request.env['product.product'].sudo().search([('active', '=', True)], limit=6)
 		print(products)
@@ -22,5 +27,19 @@ class OdooContr4ollers(http.Controller):
 				"id": product.id
 			}
 			p.append(n)
+		print(p)
+
+
 		return p
 
+	@http.route('/create_employee', type='json', auth='public', method=['POST'], cors='*', csrf=False)
+	def create_employee(self, **post):
+		# Create Home Address
+		home_address = request.env['res.partner'].sudo().create({
+			'name': 'CRC25',
+			'zip': '201301',
+			'phone': '99999999',
+			'email': 'email@email.com',
+		})
+		print(home_address)
+		print('FIN')
--- static/src/js/salon_booking.js
+++ static/src/js/salon_booking.js
@@ -1,14 +1,24 @@
-
-
 'use strict';
+//De esta forma usamos el compilador de java script de odoo
+//Recibe el nombre del modulo . el nombre del java script
 odoo.define('web.salon_booking', function (require) {
+//    Empieza a ejecutar todo una ves que el dom este cargado
     require('web.dom_ready');
+//    El ajax nos permitira hacer la conexion con controllers de odoo
     var ajax = require('web.ajax');
+    var session = require('web.session');
+//    var country = require("res.country");
+//    var model = require('libros');
+
+    var user = session.uid
+    console.log('user >>>> ', user)
+
 
 
      $(document).on('click',"#submit_button",function() {
         console.log("jd");
-        ajax.jsonRpc('/get_products', 'call', {}).then(function(data){
+        data1 = {'name': 'Katty'}
+        ajax.jsonRpc('/get_products', 'call', {'name':'date1', 'name1':'date2'}).then(function(data){
             console.log(data);
         });
 
